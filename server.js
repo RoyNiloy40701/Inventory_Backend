f906970f489e3678921b2520a4e1cc1a3156f3a0
RoyNiloy40701 <niloyroyaiub@gmail.com>
2025-09-16 20:48:27 +0600

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,2 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const connectDB = require('./config/db');
 const app = require('./app');
-
-const PORT = process.env.PORT || 5000;
-
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
-
+module.exports = app;
