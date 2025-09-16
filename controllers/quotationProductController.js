eecf97b5df45cf01a5b2ae617a5e5a5478e66071
RoyNiloy40701 <niloyroyaiub@gmail.com>
2025-09-16 23:01:22 +0600

    part yadd p2

--- controllers/quotationProductController.js
+++ controllers/quotationProductController.js
@@ -1,4 +1,5 @@
-const Product = require('../models/Product');
+
+const Product = require('../models/product');
 const QuotationProduct = require('../models/QuotationProduct');
 
 exports.getAllQuotationProduct = async (req, res) => {
