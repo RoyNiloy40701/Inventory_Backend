a76f4f4c3fba2cc70b11e32926e836815e7e506f
RoyNiloy40701 <niloyroyaiub@gmail.com>
2025-09-16 21:36:28 +0600

    formatting donre

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -6,11 +6,11 @@ const generateSerialId = require('../utils/generateSerialId');
 exports.createEmployee = async (req, res) => {
   try {
     const { serialNo, id } = await generateSerialId({
-          compid: req.body.compid,
-          Model: Employee,
-          prefixLetter: "Emp",
-        });
-        req.body.empid = id;
+      compid: req.body.compid,
+      Model: Employee,
+      prefixLetter: "Emp",
+    });
+    req.body.empid = id;
     const employee = await Employee.create(req.body)
     res.status(201).json(employee)
   } catch (error) {
@@ -23,25 +23,25 @@ exports.getAllEmployee = async (req, res) => {
     const employees = await Employee.find().sort({ regdate: -1 })
 
     const employeeDetails = await Promise.all(
-          employees.map(async (employee) => {
-            const departments = await Department.findById({
-              _id: employee?.dpt_id,
-            });
-    
-            const roles = await Role.findById({
-              _id: employee?.role_id,
-             
-            });
-    
-            return {
-              ...employee.toObject(),
-              departmentName: departments ? departments?.dept_name : null,
-              roleName: roles ? roles?.levelName : null,
-              
-            };
-          })
-        );
-    res.status(200).json({data:employeeDetails})
+      employees.map(async (employee) => {
+        const departments = await Department.findById({
+          _id: employee?.dpt_id,
+        });
+
+        const roles = await Role.findById({
+          _id: employee?.role_id,
+
+        });
+
+        return {
+          ...employee.toObject(),
+          departmentName: departments ? departments?.dept_name : null,
+          roleName: roles ? roles?.levelName : null,
+
+        };
+      })
+    );
+    res.status(200).json({ data: employeeDetails })
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
@@ -54,7 +54,7 @@ exports.getEmployeeById = async (req, res) => {
     if (!employee) {
       return res.status(404).json({ message: 'Employee not found' })
     }
-    res.status(200).json({data:employee})
+    res.status(200).json({ data: employee })
 
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -67,13 +67,12 @@ exports.updateEmployee = async (req, res) => {
     if (!employee) {
       return res.status(404).json({ message: 'Employee not found' })
     }
-    res.status(200).json({data:employee})
+    res.status(200).json({ data: employee })
 
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 }
-
 exports.deleteEmployee = async (req, res) => {
   try {
     const employee = await Employee.findByIdAndDelete(req.params.id)
